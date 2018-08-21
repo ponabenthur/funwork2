f47f22e956b7f1668721bfe5cd4ee73540626bb1
ponabenthur <vithushan302@gmail.com>
2018-08-21 06:32:54 +0530

    simple

---
+++ config/db.js
@@ -0,0 +1,4 @@
+module.exports ={
+//  url: "mongodb://localhost:27017/simpleapi"
+  url: "mongodb://vithushan:vithu1995@ds221242.mlab.com:21242/codexive"
+}
---
+++ server.js
@@ -0,0 +1,18 @@
+const express        = require('express');
+const MongoClient    = require('mongodb').MongoClient;
+const bodyParser     = require('body-parser');
+const db             = require('./config/db');
+const app            = express();
+const port           = process.env.PORT || 8000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+MongoClient.connect(db.url, (err, database) => {
+  if (err) return console.log(err)
+
+  database = database.db("simpleapi")
+  require('./app/routes')(app, database);
+  app.listen(port, () => {
+    console.log('We are live on ' + port);
+  });
+})
